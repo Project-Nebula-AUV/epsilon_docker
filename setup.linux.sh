aa74ded70ee3d92f5a7c87e8fb1221a2b50c96a9
1unarzDev <lunarzDev@outlook.com>
2026-04-12 21:16:15 +0000

    bruh

--- setup.linux.sh
+++ setup.linux.sh
@@ -278,7 +278,9 @@ setup_headless_devcontainer() {
         mkdir -p "$NVM_DIR"
         curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
         export NVM_DIR="$HOME/.nvm"
+        set +u
         [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+        set -u
     else
         echo "NVM installation found."
     fi
